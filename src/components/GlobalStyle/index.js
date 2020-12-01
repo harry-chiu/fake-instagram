03d1274deb60e3cfe970225c9120ef19063d1c35
harry.chiu <harry.chiu@jkos.com>
2020-12-01 17:25:54 +0800

    fix: add ping-fang-tc font

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+    font-family: PingFang TC, -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
 
     &:focus {
       outline: none;
