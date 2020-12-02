4aaac9546bdefded588f1d8acbba824ce080a893
harry.chiu <harry.chiu@jkos.com>
2020-12-02 16:06:08 +0800

    fix: add margin-bottom as safe-area

--- src/containers/Home/style.js
+++ src/containers/Home/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding-bottom: 64px;
+  margin-bottom: env(safe-area-inset-bottom);
 
   & > div {
     margin-top: 8px;
