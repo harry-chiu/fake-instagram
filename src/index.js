c4458760ab2f354a788d40fb3f65e6ec6855ba51
harry.chiu <harry.chiu@jkos.com>
2020-12-02 16:51:19 +0800

    fix: update manifest config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/app';
 import App from './containers/App';
 import 'regenerator-runtime';
 
+// 註冊 Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(FIREBASE_CONFIG);
 }
@@ -13,6 +14,20 @@ const container = document.getElementById('root');
 
 render(element, container);
 
+// 註冊 PWA
+
+let someshit = null;
+window.addEventListener('beforeinstallprompt', event => {
+  alert('Hello');
+  event.preventDefault();
+
+  someshit = event;
+  console.log(someshit);
+
+  return false;
+});
+
+// 註冊 Service Worker
 if (ENV === 'production' && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js');
 }
