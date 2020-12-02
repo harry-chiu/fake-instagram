ebe7b652e1f30320a442d6571b3fcefea894da1c
harry.chiu <harry.chiu@jkos.com>
2020-12-02 16:55:13 +0800

    fix: remove some pwa pattern

--- src/index.js
+++ src/index.js
@@ -14,19 +14,6 @@ const container = document.getElementById('root');
 
 render(element, container);
 
-// 註冊 PWA
-
-let someshit = null;
-window.addEventListener('beforeinstallprompt', event => {
-  alert('Hello');
-  event.preventDefault();
-
-  someshit = event;
-  console.log(someshit);
-
-  return false;
-});
-
 // 註冊 Service Worker
 if (ENV === 'production' && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js');
